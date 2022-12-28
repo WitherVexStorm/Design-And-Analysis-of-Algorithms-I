c0ddc13ad2f88a886cd903be2baecd9e6c540e7a
Jaideep Shekhar <jaideep.shekhar21@gmail.com>
2022-12-29 00:31:02 +0530

    Completed unweighted interval scheduling, started weighted interval scheduling and bipartite matching

---
+++ algorithms/greedy_algorithms/weighted_interval_scheduling/fastest_time_first.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iomanip>
+#include <utility>
+// #include <limits>
+
+struct interval {
+    int start_time;
+    int finish_time;
+    int weight;
+};
+
+bool operator >(const interval& i1, const interval& i2) {
+    return i1.finish_time > i2.finish_time;
+}
+
+template< typename T >
+void insertion_sort(T arr[], const int length) {
+    for(int i = 1, j; i < length; ++i) {
+        const T element = std::move(arr[i]);
+        for(j = i-1; j >= 0 && arr[j] > element; --j) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = std::move(element);
+    }
+}
+
+int print_interval_array(const std::string title, interval arr[], const int length, int max_finish_time = 0) {
+    bool calc_max_time = max_finish_time == 0;
+    std::cout << "\n" << title << "\n";
+    for(int i = 0, j; i < length; ++i) {
+        if(calc_max_time && arr[i].finish_time > max_finish_time) {
+            max_finish_time = arr[i].finish_time;
+        }
+        std::cout << "i = " << std::setw(2) << i << " |";
+        for(j = 0; j < arr[i].start_time; ++j)
+        std::cout << "    ";
+        const int padding = (arr[i].finish_time - arr[i].start_time)*2 - 1;
+        std::cout << std::setfill(char(178)) << std::setw(padding+2) << arr[i].weight << std::setw(padding) << "\n" << std::setfill(' ');
+    }
+    std::cout << std::setw(8) << "+";
+    for(int i = 0; i < max_finish_time; ++i)
+    std::cout << "----";
+    std::cout << "\n" << std::setw(8) << "time";
+    for(int i = 0; i < max_finish_time; ++i)
+    std::cout << std::setw(4) << i;
+    std::cout << "\n";
+    return max_finish_time;
+}
+
+// TODO: Apply max(d-tracks) solution
+int calculate_optimal(interval arr[], const int length, int& weighted_sum) {
+    int optimal_solution = 0, i = 0, j;
+    while(i < length) {
+        // Pick a[i] as part of optimal solution
+        optimal_solution += 1;
+        weighted_sum += arr[i].weight;
+        // Skip all a[j] that conflict with a[i]
+        for(j = i+1; j < length && arr[j].start_time < arr[i].finish_time; ++j);
+        // Update i to pick next a[i]
+        i = j;
+    }
+    return optimal_solution;
+}
+
+int main() {
+    interval interval_arr[] = { interval{ 5, 9, 20 }, interval{ 4, 6, 100 }, interval{ 2, 5, 70 }, interval{ 4, 8, 50 } };
+    const int interval_arr_length = sizeof(interval_arr)/sizeof(interval);
+    // int min_start_time = std::numeric_limits<int>::max();
+    const int max_finish_time = print_interval_array("Input Intervals:", interval_arr, interval_arr_length);
+    insertion_sort(interval_arr, interval_arr_length);
+    int weighted_sum = 0;
+    print_interval_array("Sorted Intervals:", interval_arr, interval_arr_length, max_finish_time);
+    const int optimal_result = calculate_optimal(interval_arr, interval_arr_length, weighted_sum);
+    std::cout << "\nOptimal solution can schedule " << optimal_result << " intervals and weighted sum = " << weighted_sum << "!";
+    return 0;
+}
